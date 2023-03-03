bd7c215d05b99e93c59c66ddd905726825daabd8
sanjanamalkar <sanjanamalkar@gmail.com>
2023-03-03 21:11:02 +0530

    present absent added

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,9 +1,20 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee\n" +
                 "Wage Computation\n" +
                 "Program on Master Branch");
+        Random num=new Random();
+        int present = num.nextInt(2);
+//        System.out.println(present);
+
+        if(present==1){
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("Employee is absent");
+        }
     }
 }
