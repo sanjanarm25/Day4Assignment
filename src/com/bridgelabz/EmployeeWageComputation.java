df20bcaeaf29c49e8a6faad314b88bf877e12f69
sanjanamalkar <sanjanamalkar@gmail.com>
2023-03-03 21:54:29 +0530

    Added:Part time and wages

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -8,17 +8,23 @@ public class EmployeeWageComputation {
                 "Wage Computation\n" +
                 "Program on Master Branch");
         Random num=new Random();
-        int present = num.nextInt(2);
+        int present = num.nextInt(3);
 //        System.out.println(present);
+        int workingHrs;
 
         if(present==1){
-            System.out.println("Employee is present");
+            System.out.println("Employee is Full timer");
+            workingHrs=16;
+        }else if(present==0) {
+            System.out.println("Employee is Part timer");
+            workingHrs=8;
+
         }else{
-            System.out.println("Employee is absent");
+            System.out.println("employee is absent");
+            workingHrs=0;
         }
         int wagePerHr=20;
-        int fullDayHr=8;
-        int DailyWages =wagePerHr*fullDayHr;
+        int DailyWages =wagePerHr*workingHrs;
         System.out.println("Daily employee wage is: " +DailyWages);
 
     }
