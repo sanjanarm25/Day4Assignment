f70af71b400c1e3d2c806a66a0268ea339a97cd0
sanjanamalkar <sanjanamalkar@gmail.com>
2023-03-03 22:03:34 +0530

    Added:Switch case

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -4,28 +4,31 @@ import java.util.Random;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee\n" +
-                "Wage Computation\n" +
-                "Program on Master Branch");
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        final int ABSENT = 0;
+
         Random num=new Random();
         int present = num.nextInt(3);
 //        System.out.println(present);
-        int workingHrs;
+        int workingHrs = 0;
 
-        if(present==1){
-            System.out.println("Employee is Full timer");
-            workingHrs=16;
-        }else if(present==0) {
-            System.out.println("Employee is Part timer");
-            workingHrs=8;
-
-        }else{
-            System.out.println("employee is absent");
-            workingHrs=0;
+        switch (present){
+            case FULL_TIME:
+                System.out.println("Employee is Full timer");
+                workingHrs=16;
+                break;
+            case PART_TIME:
+                System.out.println("Employee is Part timer");
+                workingHrs=8;
+                break;
+            case ABSENT:
+                System.out.println("employee is absent");
+                workingHrs=0;
         }
         int wagePerHr=20;
         int DailyWages =wagePerHr*workingHrs;
+        int MonthlyWages=DailyWages*20;
         System.out.println("Daily employee wage is: " +DailyWages);
-
     }
 }
