3b4a29657436644633e170a0c170286eaf01435d
sanjanamalkar <sanjanamalkar@gmail.com>
2023-03-08 20:07:53 +0530

    Added:class methods added

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -3,20 +3,19 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeeWageComputation {
-    public static void main(String[] args) {
-        final int PART_TIME = 1;
-        final int FULL_TIME = 2;
-        final int ABSENT = 0;
-        final int wagePerHr=20;
-        final int WORKING_DAYS = 20;
-        final int MAX_WORKING_HRS = 100;
-        int totalWage=0;
-        for (int day=1 ;day<=WORKING_DAYS;day++) {
+    final int PART_TIME = 1;
+    final int FULL_TIME = 2;
+    final int ABSENT = 0;
+    final int wagePerHr = 20;
+    final int workingDays = 20;
+    final int maxWorkingHrs = 100;
+    int totalWage = 0;
+
+    public int computeWage() {
+        int workingHrs = 0;
+        for (int day = 1; day <= workingDays; day++) {
             Random num = new Random();
             int present = num.nextInt(3);
-//        System.out.println(present);
-            int workingHrs = 0;
-
             switch (present) {
                 case FULL_TIME:
                     System.out.println("Employee is Full timer");
@@ -29,14 +28,20 @@ public class EmployeeWageComputation {
                 case ABSENT:
                     System.out.println("employee is absent");
                     workingHrs = 0;
+                    break;
             }
-
-            int Wages = wagePerHr * workingHrs;
-
-            System.out.println("Day " + day + " wage is:" + Wages);
-            totalWage += Wages;
-
+            int wages = wagePerHr * workingHrs;
+            System.out.println("Day " + day + " wage is:" + wages);
+            totalWage += wages;
         }
+        return totalWage;
+    }
+
+    public static void main(String[] args) {
+        EmployeeWageComputation empWage = new EmployeeWageComputation();
+        int totalWage = empWage.computeWage();
         System.out.println("Total Wage for a Month is: " + totalWage);
     }
+
+
 }
